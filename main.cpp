61c33aa9085d9a788cc61f728112face1bc15ada
rochelsl <lrochels@yahoo.de>
2026-04-29 12:54:29 +0200

    Added a temperature thermostat (Langevin model) and a bit more realistic nanoparticle magnetic moments and sizes

--- main.cpp
+++ main.cpp
@@ -15,10 +15,32 @@ const float epsilon = 1.0f;
 const float sigma = 10.0f;
 const float rc = 1.0f * sigma;
 const float rc2 = rc * rc;
+
+//for quantitative real NP behavior
+constexpr double metersPerPixel = 1e-9; // 1 pixel = 1 nm 
+float radiusPixels = 5.0f; //radius of the nanoparticles
+
+constexpr double kB   = 1.380649e-23;        // J/K
+constexpr double mu0  = 4.0 * PI * 1e-7;     // N/A^2
+constexpr double muB  = 9.2740100783e-24;    // A m^2
+
+constexpr double T = 300.0;                  // K
+constexpr double eta = 1.0e-3;               // Pa s, water
+
+constexpr double particleDiameter = 10e-9;   // 10 nm
+constexpr double radius = particleDiameter / 2.0;
+
+constexpr double magneticMoment = 1e5 * muB; // A m^2
+//for a sphere in water
+constexpr double gammaTrans = 6.0 * PI * eta * radius;
+//rotational drag
+constexpr double gammaRot = 8.0 * PI * eta * radius * radius * radius;
+
 //dipole strength
 const float dipoleStrength = 100.0f;
 std::uniform_real_distribution<float> angleDist(0.f, 2.f * PI);
-const float rotationalDamping = 0.999; // To decrease probability of numerical explosion
+//to be deactivated when Maxwe--Boltzmann temperature is included
+//const float rotationalDamping = 0.999; // To decrease probability of numerical explosion
 //Background field
 sf::Vector2f normalize(sf::Vector2f v) {
     float len = std::sqrt(v.x * v.x + v.y * v.y);
@@ -26,8 +48,14 @@ sf::Vector2f normalize(sf::Vector2f v) {
         return {0.f, 0.f};
     return v / len;
 }
-const sf::Vector2f externalFieldDirection = normalize({1.0f, 0.2f});
+const sf::Vector2f externalFieldDirection = normalize({1.0f, 0.0f});
 const float externalFieldStrength = 100.0f;
+
+//for temperature and Langevin behavior
+const float temperature = 0.2f;
+const float gamma = 0.5f;
+const float gammaRot = 0.5f;
+const float mass = 1.0f;
 
 //For grid list, performance boost
 float cellSize = rc;
@@ -39,13 +67,20 @@ struct Particle {
     sf::Vector2f position;
     sf::Vector2f velocity;
     sf::Vector2f acceleration;
-
+    //magnetic moments
     sf::Vector2f magneticMoment;
     float angle;
     float angularVelocity;
     float angularAcceleration;
 
     float radius;
+    //temperature and Langevin behavior
+    float mass;
+    float momentOfInertia;
+
+    //for the Brownian translational step of NPs moving in water, the force acting on the particle needs to be stored
+    float torque;
+    sf::Vector2f force;
 };
 
 //random magnetization vector orientation initialization
@@ -221,10 +256,15 @@ void computeForces(std::vector<Particle>& particles) {
                             sf::Vector2f B_ext = externalFieldStrength * externalFieldDirection;
                             for (auto& p : particles) {
                                 float externalTorque = cross2D(p.magneticMoment, B_ext);
-                                p.angularAcceleration += externalTorque;
+                                //magnetic field only
+                                //p.angularAcceleration += externalTorque;
+                                //magnetic field and temperature
+                                p.angularAcceleration += externalTorque / p.momentOfInertia;
                             }
-                            p1.angularAcceleration += torque1; //This assumed moment of inertia = 1: angularAcceleration += torque / 1
-                            p2.angularAcceleration += torque2;
+                            //p1.angularAcceleration += torque1; //This assumed moment of inertia = 1: angularAcceleration += torque / 1
+                            //p2.angularAcceleration += torque2;
+                            p1.angularAcceleration += torque1 / p1.momentOfInertia;
+                            p2.angularAcceleration += torque2 / p2.momentOfInertia;
 
                             float r2 = r.x * r.x + r.y * r.y;
                             if (r2 > rc2 || r2 < 1e-6f) continue;
@@ -250,8 +290,10 @@ void computeForces(std::vector<Particle>& particles) {
                             sf::Vector2f totalForce = ljForce + dipoleForce;
 
                             // Newton's third law
-                            p1.acceleration -= totalForce;
-                            p2.acceleration += totalForce;
+                            //p1.acceleration -= totalForce;
+                            //p2.acceleration += totalForce;
+                            p1.acceleration -= totalForce / p1.mass;
+                            p2.acceleration += totalForce / p2.mass;
                         }
                     }
                 }
@@ -278,6 +320,79 @@ void integrate(std::vector<Particle>& particles, float dt) {
                  + 0.5f * p.angularAcceleration * dt * dt;
 
         p.angularVelocity += 0.5f * p.angularAcceleration * dt;
+
+        p.magneticMoment = momentFromAngle(p.angle);
+    }
+}
+
+void applyLangevinThermostat(
+    std::vector<Particle>& particles,
+    float dt,
+    float temperature,
+    float gamma,
+    std::mt19937& rng
+) {
+    std::normal_distribution<float> normal(0.f, 1.f);
+
+    for (auto& p : particles) {
+        float noiseScale = std::sqrt(2.f * gamma * temperature * dt / p.mass);
+
+        p.velocity.x += -gamma * p.velocity.x * dt + noiseScale * normal(rng);
+        p.velocity.y += -gamma * p.velocity.y * dt + noiseScale * normal(rng);
+    }
+}
+
+void applyRotationalLangevinThermostat(
+    std::vector<Particle>& particles,
+    float dt,
+    float temperature,
+    float gammaRot,
+    std::mt19937& rng
+) {
+    std::normal_distribution<float> normal(0.f, 1.f);
+
+    for (auto& p : particles) {
+        float noiseScale = std::sqrt(
+            2.f * gammaRot * temperature * dt / p.momentOfInertia
+        );
+
+        p.angularVelocity += -gammaRot * p.angularVelocity * dt
+                           + noiseScale * normal(rng);
+    }
+}
+
+void brownianStep(
+    std::vector<Particle>& particles,
+    double dt,
+    std::mt19937& rng
+) {
+    std::normal_distribution<double> normal(0.0, 1.0);
+
+    const double Dt = kB * T / gammaTrans;
+    const double noiseScale = std::sqrt(2.0 * Dt * dt);
+
+    for (auto& p : particles) {
+        p.position.x += static_cast<float>((p.force.x / gammaTrans) * dt
+                      + noiseScale * normal(rng));
+
+        p.position.y += static_cast<float>((p.force.y / gammaTrans) * dt
+                      + noiseScale * normal(rng));
+    }
+}
+
+void brownianRotationStep(
+    std::vector<Particle>& particles,
+    double dt,
+    std::mt19937& rng
+) {
+    std::normal_distribution<double> normal(0.0, 1.0);
+
+    const double Dr = kB * T / gammaRot;
+    const double noiseScale = std::sqrt(2.0 * Dr * dt);
+
+    for (auto& p : particles) {
+        p.angle += static_cast<float>((p.torque / gammaRot) * dt
+                 + noiseScale * normal(rng));
 
         p.magneticMoment = momentFromAngle(p.angle);
     }
@@ -335,12 +450,16 @@ int main() {
 
     std::vector<Particle> particles;
 
-    const int N = 100;
+    const int N = 1000;
     particles.reserve(N);
 
     std::mt19937 rng(std::random_device{}());
     std::uniform_real_distribution<float> jitter(-1.0f, 1.0f);
-    std::uniform_real_distribution<float> vel(-100.f, 100.f);
+    //for random distribution of velocities
+    //std::uniform_real_distribution<float> vel(-10.f, 10.f);
+    //for Maxwell-Boltzman-like distribution of velocities
+    std::normal_distribution<float> velDist(0.f, std::sqrt(temperature / mass));
+
 
     // Grid initialization avoids catastrophic LJ overlaps from random placement.
     int cols = static_cast<int>(std::ceil(std::sqrt(N * static_cast<float>(width) / height)));
@@ -353,15 +472,28 @@ int main() {
         int iy = i / cols;
 
         Particle p;
-        p.radius = 10.f;
+        p.radius = radiusPixels;
         p.position = {(ix + 0.5f) * dx + jitter(rng), (iy + 0.5f) * dy + jitter(rng)};
-        p.velocity = {vel(rng), vel(rng)};
-        p.acceleration = {0.f, 0.f};
+        //for random distribution of velocities
+        //p.velocity = {vel(rng), vel(rng)};
+        //p.acceleration = {0.f, 0.f};
+        //for temperature and Langevin
+        p.velocity = {velDist(rng), velDist(rng)};
+        //magnetic moment
         p.magneticMoment = randomUnitVector(rng); //magnetization vector
         p.angle = angleDist(rng);
-        p.angularVelocity = 10.f;
+        //for random angular velocities
+        //p.angularVelocity = 10.f;
+        //for Maxwell-Boltzmann-like velocities
+        std::normal_distribution<float> omegaDist(
+            0.f,
+            std::sqrt(temperature / p.momentOfInertia)
+        );
+        p.angularVelocity = omegaDist(rng);
         p.angularAcceleration = 0.f;
         p.magneticMoment = momentFromAngle(p.angle);
+        //temperature and Langevin behavior
+        p.momentOfInertia = 0.5f * p.mass * p.radius * p.radius;
         particles.push_back(p);
     }
 
@@ -404,11 +536,15 @@ int main() {
         // 3. finish velocity update
         for (auto& p : particles) {
             p.velocity += 0.5f * p.acceleration * dt;
-            // For magnetization vector
             p.angularVelocity += 0.5f * p.angularAcceleration * dt;
-            p.angularVelocity *= rotationalDamping; //Include a damping
+
             p.magneticMoment = momentFromAngle(p.angle);
+
+            applyPBC(p.position, width, height);
         }
+
+        applyLangevinThermostat(particles, dt, temperature, gamma, rng);
+        applyRotationalLangevinThermostat(particles, dt, temperature, gammaRot, rng);
         // 4. walls
         // for (auto& p : particles)
         //     handleWallCollision(p, window.getSize());
@@ -429,18 +565,18 @@ int main() {
         window.draw(line, 2, sf::Lines);
         }
 
-        // sf::Vertex fieldLine[] = {
-        //     sf::Vertex({50.f, 50.f}, sf::Color::Blue),
-        //     sf::Vertex({50.f, 50.f} + externalFieldDirection * 80.f, sf::Color::Blue)
-        // };
+        sf::Vertex fieldLine[] = {
+            sf::Vertex(sf::Vector2f(50.f, 50.f), sf::Color::Blue),
+            sf::Vertex(sf::Vector2f(50.f, 50.f) + externalFieldDirection * 80.f, sf::Color::Blue)
+        };
 
-        // window.draw(fieldLine, 2, sf::Lines);
+        window.draw(fieldLine, 2, sf::Lines);
 
         window.display();
 
-        std::cout << particles[0].angle << " "
-          << particles[0].angularVelocity << " "
-          << particles[0].angularAcceleration << "\n";
+        // std::cout << particles[0].angle << " "
+        //   << particles[0].angularVelocity << " "
+        //   << particles[0].angularAcceleration << "\n";
 
         //For radial distribution function
         // frameCounter++;
